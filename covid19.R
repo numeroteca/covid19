67f9e38adb560b11ee74126f0c517ebd928c13db
numeroteca <pablo@montera34.com>
2020-03-14 16:38:42 +0100

    añade gráfico lineal uci por habitantes

--- covid19.R
+++ covid19.R
@@ -455,6 +455,40 @@ data_uci %>% filter( CCAA != "Total") %>%
        caption = caption)
 dev.off()
 
+png(filename=paste("img/covid19_casos-registrados-UCI-por-comunidad-autonoma-superpuesto-per-cienmil-lineal.png", sep = ""),width = 1000,height = 700)
+data_uci %>% filter( CCAA != "Total") %>%
+  ggplot() +
+  geom_line(aes(date,per_cienmil,group=CCAA, color=CCAA), size= 1 ) +
+  geom_point(aes(date,per_cienmil,  color=CCAA), size= 1.5 ) +
+  geom_text_repel(data=filter( data_uci, date==max(data_uci$date),  CCAA != "Total"), 
+                  aes(date,per_cienmil, color = CCAA, label=paste(format(per_cienmil, nsmall=1, big.mark="."),CCAA)),
+                  nudge_x = 3, # adjust the starting y position of the text label
+                  size=5,
+                  # hjust=0,
+                  family = "Roboto Condensed",
+                  direction="y",
+                  segment.size = 0.1,
+                  segment.color="#777777"
+  ) +
+  scale_x_date(date_breaks = "1 day", 
+               date_labels = "%d",
+               limits=c( min(data_uci$date), max(data_uci$date + 1.5)) 
+  ) + 
+  theme_minimal(base_family = "Roboto Condensed",base_size = 16) +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank(),
+    # panel.grid.minor.y = element_blank(),
+    axis.ticks.x = element_line(color = "#000000"),
+    legend.position = "none"
+  ) +
+  labs(title = "Número de personas (acumulado) en la UCI por COVID-19 registrados por 100.000 habitantes en España",
+       subtitle = paste0("Por comunidad autónoma (escala lineal). ",period),
+       y = "personas en UCI por 100.000 habitantes",
+       x = "fecha",
+       caption = caption)
+dev.off()
+
 png(filename=paste("img/covid19_casos-registrados-UCI-por-comunidad-autonoma-superpuesto-per-cienmil-log.png", sep = ""),width = 1000,height = 700)
 data_uci %>% filter( CCAA != "Total") %>%
   ggplot() +
