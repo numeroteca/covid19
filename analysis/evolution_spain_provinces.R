33c5cd2005e8c17861ea8412a9a24cf51a04c297
numeroteca <pablo@montera34.com>
2020-03-25 00:59:07 +0100

    update provincias spain data 2020.03.24

--- analysis/evolution_spain_provinces.R
+++ analysis/evolution_spain_provinces.R
@@ -33,7 +33,7 @@ data_cases_sp_provinces$deceassed_per_100000 <- round( data_cases_sp_provinces$d
 caption <- "Gráfico: lab.montera34.com/covid19 | Datos: Ministerio de Sanidad de España extraídos por Datadista.com"
 caption_en <- "By: lab.montera34.com/covid19 | Data: various official sources. Check website."
 caption_provincia <- "Gráfico: montera34.com | Datos: Varias fuentes. Ver lab.montera34.com"
-period <- "2020.02.27 - 03.23"
+period <- "2020.02.27 - 03.24"
 
 # Plots --------------------
 # / 1. Cases ------------
@@ -53,7 +53,6 @@ data_cases_sp_provinces %>%
   geom_line(aes(date,cases_accumulated,group=province) ) +
   geom_point(aes(date,cases_accumulated,group=province), size = 0.5 ) +
   facet_wrap( ~province) +
-  scale_y_continuous(
     limits = c(0,max(data_cases_sp_provinces$cases_accumulated) ),
     labels=function(x) format(round(x, digits = 0), big.mark = ".", scientific = FALSE) ) +
   scale_x_date(date_breaks = "2 day", 
@@ -306,14 +305,184 @@ dev.off()
 
 # / 2. UCI (intensive care) -------------------
 
-# // 2.1 UCI Small multiple ----------
-# Escala lineal
+# // 2.1 UCI Small multiple 
 
 # / 3. Deceassed (Fallecimientos) ------------
 
-# // 3.1 Fallecimientos Small multiple ----------
+# create temp dataframes to be able to plot all the values in small multiples
+data_cases_sp_provinces_sm <- data_cases_sp_provinces
+data_cases_sp_provinces_sm$province_cp <- data_cases_sp_provinces$province
 
-# // Provincias ----------------
+# // 3.1 Fallecimientos Small multiple ----------
+png(filename=paste("img/spain/provincias/covid19_fallecimientos-registrados-por-provincia-lineal.png", sep = ""),width = 1200,height = 800)
+data_cases_sp_provinces %>%
+  ggplot() +
+  geom_line(data = select(data_cases_sp_provinces_sm,date,deceased,province_cp,-province),
+            aes(date,deceased,group=province_cp), color="#CACACA" ) +
+  geom_line(aes(date, deceased,group=province) ) +
+  geom_point(aes(date, deceased), size= 0.5 ) +
+  facet_wrap(~province) +
+  scale_x_date(date_breaks = "3 day", 
+               date_labels = "%d",
+               limits=c( min(data_cases_sp_provinces$date), max(data_cases_sp_provinces$date + 6)) 
+  ) + 
+  theme_minimal(base_family = "Roboto Condensed",base_size = 16) +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank(),
+    # panel.grid.minor.y = element_blank(),
+    axis.ticks.x = element_line(color = "#000000"),
+    legend.position = c(0.1,0.6)
+  ) +
+  labs(title = "Número de fallecimientos acumulados por COVID-19 registrados en España",
+       subtitle = paste0("Por provincia [Cataluña sin desagregar, Canarias por islas] (escala lineal). ",period),
+       y = "fallecidos",
+       x = "fecha",
+       caption = caption)
+dev.off()
+
+png(filename=paste("img/spain/provincias/covid19_fallecimientos-registrados-por-provincia-log.png", sep = ""),width = 1200,height = 800)
+data_cases_sp_provinces %>%
+  ggplot() +
+  geom_line(data = select(data_cases_sp_provinces_sm,date,deceased,province_cp,-province),
+            aes(date,deceased,group=province_cp), color="#CACACA" ) +
+  geom_line(aes(date, deceased,group=province) ) +
+  geom_point(aes(date, deceased), size= 0.5 ) +
+  facet_wrap(~province) +
+  scale_y_log10( minor_breaks = c(seq(1 , 10, 1),seq(10 , 100, 10), seq(100 , 1000, 100)) ) +
+  scale_x_date(date_breaks = "3 day", 
+               date_labels = "%d",
+               limits=c( min(data_cases_sp_provinces$date), max(data_cases_sp_provinces$date + 6)) 
+  ) + 
+  theme_minimal(base_family = "Roboto Condensed",base_size = 16) +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank(),
+    # panel.grid.minor.y = element_blank(),
+    axis.ticks.x = element_line(color = "#000000"),
+    legend.position = c(0.1,0.6)
+  ) +
+  labs(title = "Número de fallecimientos acumulados por COVID-19 registrados en España",
+       subtitle = paste0("Por provincia [Cataluña sin desagregar, Canarias por islas] (escala logarítmica). ",period),
+       y = "fallecidos",
+       x = "fecha",
+       caption = caption)
+dev.off()
+
+png(filename=paste("img/spain/provincias/covid19_fallecimientos-registrados-por-provincia-log_ccaa.png", sep = ""),width = 1000,height = 600)
+data_cases_sp_provinces %>%
+  ggplot() +
+  geom_line(data = select(data_cases_sp_provinces_sm,date,deceased,province_cp,-province),
+            aes(date,deceased,group=province_cp), color="#CACACA" ) +
+  geom_line(aes(date, deceased,group=province) ) +
+  geom_point(aes(date, deceased), size= 0.5 ) +
+  facet_wrap(~ccaa) +
+  scale_y_log10( minor_breaks = c(seq(1 , 10, 1),seq(10 , 100, 10), seq(100 , 1000, 100)) ) +
+  scale_x_date(date_breaks = "3 day", 
+               date_labels = "%d",
+               limits=c( min(data_cases_sp_provinces$date), max(data_cases_sp_provinces$date + 6)) 
+  ) + 
+  theme_minimal(base_family = "Roboto Condensed",base_size = 16) +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank(),
+    # panel.grid.minor.y = element_blank(),
+    axis.ticks.x = element_line(color = "#000000"),
+    legend.position = c(0.1,0.6)
+  ) +
+  labs(title = "Número de fallecimientos acumulados por COVID-19 registrados en España",
+       subtitle = paste0("Por provincia [Cataluña sin desagregar, Canarias por islas] (escala logarítmica). ",period),
+       y = "fallecidos",
+       x = "fecha",
+       caption = caption)
+dev.off()
+
+png(filename=paste("img/spain/provincias/covid19_fallecimientos-registrados-por-provincia-per-cienmil-lineal.png", sep = ""),width = 1200,height = 800)
+data_cases_sp_provinces %>%
+  ggplot() +
+  geom_line(data = select(data_cases_sp_provinces_sm,date,deceassed_per_100000,province_cp,-province),
+            aes(date,deceassed_per_100000,group=province_cp), color="#CACACA" ) +
+  geom_line(aes(date, deceassed_per_100000,group=province)) +
+  geom_point(aes(date, deceassed_per_100000), size= 0.5 ) +
+  facet_wrap(~province) +
+  scale_x_date(date_breaks = "3 day", 
+               date_labels = "%d",
+               limits=c( min(data_cases_sp_provinces$date), max(data_cases_sp_provinces$date + 5)) 
+  ) + 
+  theme_minimal(base_family = "Roboto Condensed",base_size = 16) +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank(),
+    # panel.grid.minor.y = element_blank(),
+    axis.ticks.x = element_line(color = "#000000"),
+    legend.position = c(0.1,0.6)
+  ) +
+  labs(title = "Número de fallecimientos acumulados por COVID-19 registrados por 100.000 habitantes en España",
+       subtitle = paste0("Por provincia  [sin Cataluña y Canarias] (escala lineal). ",period),
+       y = "fallecidos por 100.000 habitantes",
+       x = "fecha",
+       caption = caption)
+dev.off()
+
+png(filename=paste("img/spain/provincias/covid19_fallecimientos-registrados-por-provincia-per-cienmil-log.png", sep = ""),width = 1200,height = 800)
+data_cases_sp_provinces %>%
+  ggplot() +
+  geom_line(data = select(data_cases_sp_provinces_sm,date,deceassed_per_100000,province_cp,-province),
+            aes(date,deceassed_per_100000,group=province_cp), color="#CACACA" ) +
+  geom_line(aes(date, deceassed_per_100000,group=province)) +
+  geom_point(aes(date, deceassed_per_100000), size= 0.5 ) +
+  facet_wrap(~province) +
+  scale_y_log10(  
+    limits = c(0.05,max(data_cases_sp_provinces$deceassed_per_100000)),
+    minor_breaks =  c(  seq(0.01 , 0.1, 0.01), seq(0.1 , 1, 0.1), seq(1 , 10, 1), seq(10 , 100, 10), seq(100 , 1000, 100) ) ) +
+  scale_x_date(date_breaks = "3 day", 
+               date_labels = "%d",
+               limits=c( min(data_death$date), max(data_death$date + 5)) 
+  ) + 
+  theme_minimal(base_family = "Roboto Condensed",base_size = 16) +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank(),
+    axis.ticks.x = element_line(color = "#000000"),
+    legend.position = c(0.1,0.6)
+  ) +
+  labs(title = "Número de fallecimientos acumulados por COVID-19 registrados por 100.000 habitantes en España",
+       subtitle = paste0("Por provincia [sin Cataluña y Canarias] (escala logarítmica). ",period),
+       y = "fallecidos por 100.000 habitantes",
+       x = "fecha",
+       caption = caption)
+dev.off()
+
+png(filename=paste("img/spain/provincias/covid19_fallecimientos-registrados-por-provincia-per-cienmil-log-ccaa.png", sep = ""),width = 1000,height = 600)
+data_cases_sp_provinces %>%
+  ggplot() +
+  geom_line(data = select(data_cases_sp_provinces_sm,date,deceassed_per_100000,province_cp,-province),
+            aes(date,deceassed_per_100000,group=province_cp), color="#CACACA" ) +
+  geom_line(aes(date, deceassed_per_100000,group=province)) +
+  geom_point(aes(date, deceassed_per_100000), size= 0.5 ) +
+  facet_wrap(~ccaa) +
+  scale_y_log10(  
+    limits = c(0.05,max(data_cases_sp_provinces$deceassed_per_100000)),
+    minor_breaks =  c(  seq(0.01 , 0.1, 0.01), seq(0.1 , 1, 0.1), seq(1 , 10, 1), seq(10 , 100, 10), seq(100 , 1000, 100) ) ) +
+  scale_x_date(date_breaks = "3 day", 
+               date_labels = "%d",
+               limits=c( min(data_death$date), max(data_death$date + 5)) 
+  ) + 
+  theme_minimal(base_family = "Roboto Condensed",base_size = 16) +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank(),
+    axis.ticks.x = element_line(color = "#000000"),
+    legend.position = c(0.1,0.6)
+  ) +
+  labs(title = "Número de fallecimientos acumulados por COVID-19 registrados por 100.000 habitantes en España",
+       subtitle = paste0("Por provincia [sin Cataluña y Canarias] (escala logarítmica). ",period),
+       y = "fallecidos por 100.000 habitantes",
+       x = "fecha",
+       caption = caption)
+dev.off()
+
+# // 3.2 Superpuesto----------------------
 png(filename=paste("img/spain/provincias/covid19_fallecimientos-registrados-por-provincia-superpuesto-lineal.png", sep = ""),width = 1200,height = 700)
 data_cases_sp_provinces %>%
   ggplot() +
