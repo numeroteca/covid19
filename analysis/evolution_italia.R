1f086d61486ecafcb25b97443144d5abd269c91a
numeroteca <pablo@montera34.com>
2020-03-28 19:27:14 +0100

    update Spain and Italia regions 2020.03.28

--- analysis/evolution_italia.R
+++ analysis/evolution_italia.R
@@ -8,7 +8,7 @@ library(ggrepel) # for geom_text_repel to prevent overlapping
 # Settings -------
 # Cambia el pie del gráfico pero conserva la fuente de los datos
 caption_i <- "Gráfico: @numeroteca (montera34.com). Datos: Protezione Civile (Italia)"
-periodo_i <- "2020.02.24 - 03.27"
+periodo_i <- "2020.02.24 - 03.28"
 
 # COVID-19 in Italy -----------
 # Load data
