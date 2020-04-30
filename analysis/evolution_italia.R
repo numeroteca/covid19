eca11c721e649c230f3e5f0543bd9c3cf2503e59
numeroteca <pablo@montera34.com>
2020-04-30 02:41:33 +0200

    update charts IT FR 2020.04.29

--- analysis/evolution_italia.R
+++ analysis/evolution_italia.R
@@ -8,7 +8,7 @@ library(ggrepel) # for geom_text_repel to prevent   overlapping
 # Settings -------
 # Cambia el pie del gráfico pero conserva la fuente de losS datos
 caption_i <- "Gráfico: @numeroteca (lab.montera34.com/covid19). Datos: Protezione Civile (Italia)"
-periodo_i <- "2020.02.24 - 04.28"
+periodo_i <- "2020.02.24 - 04.29"
 
 # COVID-19 in Italy -----------
 # Load data
