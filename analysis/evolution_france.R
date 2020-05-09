52e0869e02dcd2163e477f471a4bc2cf71495a2e
numeroteca <pablo@montera34.com>
2020-05-09 23:32:28 +0200

    update IT FR charts 2020.05.09

--- analysis/evolution_france.R
+++ analysis/evolution_france.R
@@ -9,7 +9,7 @@
   # Settings -------  
   # Cambia el pie del gráfico pero conserva la fuente de los datos
   caption_f <- "Gráfico: lab.montera34.com/covid19. Datos: OpenCOVID19-fr"
-  periodo_f <- "2020.03.04 - 05.08"
+  periodo_f <- "2020.03.04 - 05.09"
   
   # COVID-19 in France-----------
   
