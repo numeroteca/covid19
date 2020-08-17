838fde8cfd88b33b87cb1e01fc09de2d5fa2c665
numeroteca <pablo@montera34.com>
2020-08-17 18:32:45 +0200

    update Italia+France data-charts

--- analysis/evolution_france.R
+++ analysis/evolution_france.R
@@ -9,7 +9,7 @@ library(ggrepel) # for geom_text_repel to prevent overlapping
 # Settings -------  
 # Cambia el pie del gráfico pero conserva la fuente de los datos
 caption_f <- "Gráfico: lab.montera34.com/covid19. Datos: OpenCOVID19-fr"
-periodo_f <- "2020.03.04 - 08.07"
+periodo_f <- "2020.03.04 - 08.17"
 
 # COVID-19 in France-----------
 
