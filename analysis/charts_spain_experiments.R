70144f5bf652873f1151b0f601cb3d89ebd51d64
numeroteca <pablo@montera34.com>
2020-09-06 00:10:26 +0200

    Create Tablas report. Update Spain prov. data-charts 2020-09-04.

--- analysis/charts_spain_experiments.R
+++ analysis/charts_spain_experiments.R
@@ -4,7 +4,7 @@ data_cases_sp_provinces <- readRDS(file = "data/output/spain/covid19-provincias-
 
 noprevalentes <- c("")
 updated <- ""
-period <- "(Actualizado: 2020-09-04)"
+period <- "(Actualizado: 2020-09-05)"
 
 # 13. Todos los datos juntos ------------
 # Calcula máximos
